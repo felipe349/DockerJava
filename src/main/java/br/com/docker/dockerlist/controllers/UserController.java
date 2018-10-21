1062202c76f3b28f42ffd7c667615cedbc9c26f0
Felipe Carvalho dos Anjos Formentin <claiohm@gmail.com>
2018-10-21 20:26:38 -0200

    Added angular to container

--- src/main/java/br/com/docker/dockerlist/controllers/UserController.java
+++ src/main/java/br/com/docker/dockerlist/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://192.168.99.100:4200"})
 @RestController
 @RequestMapping("/api")
 public class UserController {
