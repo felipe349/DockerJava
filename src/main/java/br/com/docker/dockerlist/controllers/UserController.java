dee7c8b353c54042e9b94b5455e713cc79365eba
Felipe Carvalho dos Anjos Formentin <claiohm@gmail.com>
2018-10-21 22:33:00 -0200

    Check why docker swarm is not working

--- src/main/java/br/com/docker/dockerlist/controllers/UserController.java
+++ src/main/java/br/com/docker/dockerlist/controllers/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200", "http://192.168.99.100:4200"})
@@ -20,5 +23,13 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return repository.findAll();
+    }
+
+    @GetMapping("/host")
+    public List<String> getHost() throws UnknownHostException {
+        List<String> host = new ArrayList<>();
+        host.add(InetAddress.getLocalHost().getHostAddress());
+        host.add(InetAddress.getLocalHost().getHostName());
+        return host;
     }
 }
